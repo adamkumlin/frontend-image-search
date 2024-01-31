93ee3902cb731552869b18b360825f17fe7a967e
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-01-31 11:03:32 +0100

    script.js | put functions in chronological order
    functions are now arranged in the order that they are used in the program,
    
    also divided submitform and search animation handling into 2 separate functions

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // api key 
 const apikey = "42111173-a877c127734af95d5350e4bd2";
 
-// api call, returns json 
-async function apiCallJSON(query) {
-    const url = "https://pixabay.com/api/?key=" +
-        apikey +
-        query;
-    const result = await fetch(url);
-    const json = await result.json();
-
-    return json;
-}
-
 // build query, return value is meant to be passed as parameter to apiCall() 
 function buildQuery(
     searchTerm,
@@ -35,6 +24,50 @@ function buildQuery(
     return query;
 }
 
+// api call, returns json 
+async function apiCallJSON(query) {
+    const url = "https://pixabay.com/api/?key=" +
+        apikey +
+        query;
+    const result = await fetch(url);
+    const json = await result.json();
+
+    return json;
+}
+
+function getJSONFromUserInput() {
+    // get all of the info the user has submitted
+    const searchText = document.getElementById("search").value;
+    const color = document.getElementById("color").value;
+    const category = document.getElementById("category").value;
+    const imageType = document.getElementById("imageType").value;
+    const resultsPerPage = document.getElementById("resultsPerPage").value;
+    const order = document.getElementById("order").value;
+    
+    if (searchText != "") {
+        // Build query & retrieve json from api 
+        const query = 
+            buildQuery(
+                searchText, color, category, imageType, resultsPerPage, order
+            );
+        const apiCallJSON = apiCallJSON(query);
+
+        return apiCallJSON;
+    }
+    else {
+        alert("Fel: Sökfältet är tomt.")
+        return;
+    }
+}
+
+function searchAnimation() {
+    // For the search animation, add class with name "animated" to the image in the h1
+    const image = document.querySelector("h1 img");
+    image.classList.add("animated");
+    
+    // TODO: remove class when displaying images, can be done later
+}
+
 // eventListener and it's associated function below
 const mainForm = document.getElementById("form");
 mainForm.addEventListener("submit", submitForm);
@@ -42,32 +75,7 @@ mainForm.addEventListener("submit", submitForm);
 function submitForm(e) {
     e.preventDefault();
 
-    const image = document.querySelector("h1 img");
-    image.classList.add("animated");
-    // For the search animation, add class with name "animated" to the image in the h1
-
-    // TODO: remove class when displaying images, can be done later
-
-    const searchText = document.getElementById("search").value;
-    const color = document.getElementById("color").value;
-    const category = document.getElementById("category").value;
-    const imageType = document.getElementById("imageType").value;
-    const resultsPerPage = document.getElementById("resultsPerPage").value;
-    const order = document.getElementById("order").value;
-
-    if (searchText != "") {
-        // Build query
-        const testQuery = 
-            buildQuery(
-                searchText, color, category, imageType, resultsPerPage, order
-            );
-        const testApiCallJSON = apiCallJSON(testQuery);
-        console.log(testQuery);
-        console.log(testApiCallJSON);
-    }
-    else {
-        alert("Fel: Sökfältet är tomt.")
-        return;
-    }
+    searchAnimation();
+    const pictureJSON = getJSONFromUserInput();
 }
 
